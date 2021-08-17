0ca936a9adbaad35999504f0c4f2afe6b9ead820
Rajat Kumar <53110995+rajat044@users.noreply.github.com>
2021-08-17 10:07:42 +0530

    Update extract_digits.py

--- extract_digits.py
+++ extract_digits.py
@@ -5,12 +5,14 @@ import numpy as np
 from keras.models import model_from_json
 
 # Load the saved model
-json_file = open('Model/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("Model/model.h5")
+# json_file = open('Model/model.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# loaded_model = model_from_json(loaded_model_json)
+# # load weights into new model
+# loaded_model.load_weights("Model/model.h5")
+model_path = 'Model/digitRecognition.h5'
+emotion_model = load_model(model_path)
 print("Loaded saved model from disk.")
  
 # evaluate loaded model on test data

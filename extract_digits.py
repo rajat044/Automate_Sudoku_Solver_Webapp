2588613e3bd3f2389db01eb564ed096b800b7c7b
Rajat Kumar <53110995+rajat044@users.noreply.github.com>
2021-08-17 09:46:16 +0530

    Update extract_digits.py

--- extract_digits.py
+++ extract_digits.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import model_from_json
 
-#Here I am using a pre-trained model to extracts digits from the image
 # Load the saved model
 json_file = open('Model/model.json', 'r')
 loaded_model_json = json_file.read()
@@ -18,22 +17,21 @@ print("Loaded saved model from disk.")
 def identify_number(image):
     image_resize = cv2.resize(image, (28,28))    # For plt.imshow
     image_resize_2 = image_resize.reshape(1,1,28,28)    # For input to model.predict_classes
+#   cv2.imshow('number', image_test_1)
     loaded_model_pred = loaded_model.predict_classes(image_resize_2 , verbose = 0)
+#    print('Prediction of loaded_model: {}'.format(loaded_model_pred[0]))
     return loaded_model_pred[0]
 
 def ExtractDigits(sudoku):
     sudoku = cv2.resize(sudoku, (450,450))
-
 
     # split sudoku
     grid = np.zeros([9,9])
     for i in range(9):
         for j in range(9):
             image = sudoku[i*50:(i+1)*50,j*50:(j+1)*50]
-            image
             if image.sum() > 25000:
-                #grid[i][j] = identify_number(image)
-                grid[i][j] = 0
+                grid[i][j] = identify_number(image)
             else:
                 grid[i][j] = 0
     return grid.astype(int)

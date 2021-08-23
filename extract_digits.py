d52a3fd1eef42f4ef6dfa0413a199e370e0efc56
Rajat Kumar <53110995+rajat044@users.noreply.github.com>
2021-08-23 13:37:22 +0530

    Update extract_digits.py

--- extract_digits.py
+++ extract_digits.py
@@ -5,13 +5,13 @@ import numpy as np
 from keras.models import model_from_json, load_model
 
 # Load the saved model
-json_file = open('Model/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("Model/model.h5")
-
+# json_file = open('Model/model.json', 'r')
+# loaded_model_json = json_file.read()
+# json_file.close()
+# loaded_model = model_from_json(loaded_model_json)
+# # load weights into new model
+# loaded_model.load_weights("Model/model.h5")
+loaded_model = load_model("Model/model.h5")
 print("Loaded saved model from disk.")
  
 # evaluate loaded model on test data

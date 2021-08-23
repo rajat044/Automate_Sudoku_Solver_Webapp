997fa7ffd00002cf8cdcd8c6d8110f2c0dcb19e2
Rajat Kumar <53110995+rajat044@users.noreply.github.com>
2021-08-23 14:09:44 +0530

    Update extract_numbers.py

--- extract_numbers.py
+++ extract_numbers.py
@@ -1,25 +1,20 @@
 import cv2
 import numpy as np
 from keras.models import load_model
-import matplotlib.pyplot as plt
+from img_preprocessing import scale_and_centre,  order_corner_points
 
 
 def predict(img_grid):
     image = img_grid.copy()
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # image = cv2.threshold(image, 140, 255, cv2.THRESH_BINARY)[1]
+    
     image = cv2.resize(image, (28, 28))
-    # display_image(image)
+    
     image = image.astype('float32')
     image = image.reshape(1, 28, 28, 1)
     image /= 255
-
-    # plt.imshow(image.reshape(28, 28), cmap='Greys')
-    # plt.show()
+    
     model = load_model('model.h5')
     pred = model.predict(image.reshape(1, 28, 28, 1), batch_size=1)
-
-    # print(pred.argmax())
 
     return pred.argmax()
 
@@ -51,10 +46,7 @@ def extract_number_image(img_grid):
                     continue
                 ROI = gray[y:y + h, x:x + w]
                 ROI = scale_and_centre(ROI, 120)
-                # display_image(ROI)
-
-                # Writing the cleaned cells
-                cv2.imwrite("CleanedBoardCells/cell{}{}.png".format(i, j), ROI)
+                
                 tmp_sudoku[i][j] = predict(ROI)
 
     return tmp_sudoku
